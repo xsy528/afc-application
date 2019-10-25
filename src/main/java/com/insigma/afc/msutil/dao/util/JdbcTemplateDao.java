27d5e2b7f279a8dabfb99077c44b9d3134603e95
xingshaoya <xingshaoya@unittec.com>
2019-10-25 16:27:10 +0800

    去除无用类

---
+++ src/main/java/com/insigma/afc/msutil/dao/util/JdbcTemplateDao.java
@@ -0,0 +1,367 @@
+package com.insigma.afc.msutil.dao.util;
+
+
+import com.insigma.commons.util.DateTimeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.CallableStatementCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Method;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Ticket: Jdbc工具类
+ *
+ * @author: xingshaoya
+ * @time: 2019-07-09 11:18
+ */
+@Service
+public class JdbcTemplateDao {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTemplateDao.class);
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * 只查询一列数据类型对象。用于只有一行查询结果的数据
+     * @param sql
+     * @param params
+     * @param cla Integer.class,Float.class,Double.Class,Long.class,Boolean.class,Char.class,Byte.class,Short.class
+     * @return
+     */
+    private Object queryOneColumnForSigetonRow(String sql,Object[] params,Class cla){
+
+        try{
+            if(params!=null&&params.length>0) {
+
+                return jdbcTemplate.queryForObject(sql, params, cla);
+            }
+            return jdbcTemplate.queryForObject(sql,cla);
+        }catch(Exception ex){
+            LOGGER.error(ex.toString());
+            return null;
+        }
+    }
+
+
+    /**
+     * 更新表
+     * @param sql
+     * @param params
+     * @return
+     */
+    public Integer update(String sql,Object[] params){
+        try{
+            if(params!=null&&params.length>0) {
+                return jdbcTemplate.update(sql, params);
+            }
+            return jdbcTemplate.update(sql);
+        }catch(Exception ex){
+            LOGGER.error(ex.toString());
+            return null;
+        }
+    }
+
+    /**
+     * 删除数据
+     * @param sql
+     * @param params
+     * @return
+     */
+    public Integer delect(String sql,Object[] params){
+       return update(sql,params);
+    }
+
+    /**
+     * 获取存取过程的结果-时间
+     * @param sql
+     * @param type
+     * @param params
+     * @return
+     */
+    public Date excuteSql(String sql,int type,Object... params){
+        Object execute = jdbcTemplate.execute(con -> {
+            CallableStatement cs = con.prepareCall(sql);
+            //cs.setObject(1,obj[0]);// 设置输入参数的值
+            cs.setObject(1,params[0]);
+            cs.registerOutParameter(2,type);// 注册输出参数的类型
+            return cs;
+        }, (CallableStatementCallback) cs -> {
+            cs.execute();
+            return cs.getObject(2);// 获取输出参数的值
+        });
+        return (Date)execute;
+
+    }
+    /**
+     * 返回结果为单个String
+     * @param sql
+     * @param params
+     * @param getName
+     * @return
+     */
+    public String queryForString(String sql,Object[] params,String getName){
+        return queryForMaps(sql, params).get(0).get(getName)+"";
+    }
+
+    public Date queryForDate(String sql, Object[] params){
+        List<Map<String, Object>> maps = queryForMaps(sql, params);
+
+        String strDate = maps.get(0) + "";
+        strDate = strDate.substring(10);
+        Date strtodate = DateTimeUtil.stringToDate(strDate,"yyyy-MM-dd");
+
+        return strtodate;
+    }
+    /**
+     * 查询返回实体对象集合
+     * @param sql    sql语句
+     * @param params 填充sql问号占位符数
+     * @param cla    实体对象类型
+     * @return
+     */
+    public List queryForObjectList(String sql,Object[] params,final Class cla){
+        final List list=new ArrayList();
+        try{
+            jdbcTemplate.query(sql, params, new RowCallbackHandler(){
+                @Override
+                public void processRow(ResultSet rs) {
+                    try{
+                        List<String> columnNames=new ArrayList<String>();
+                        ResultSetMetaData meta=rs.getMetaData();
+                        int num=meta.getColumnCount();
+                        for(int i=0;i<num;i++){
+                            columnNames.add(meta.getColumnLabel(i+1).toLowerCase().trim());
+                        }
+                        Method[] methods=cla.getMethods();
+                        List<String> fields=new ArrayList<String>();
+                        for(int i=0;i<methods.length;i++){
+                            if(methods[i].getName().trim().startsWith("set")){
+                                String f=methods[i].getName().trim().substring(3);
+                                f=(f.charAt(0)+"").toLowerCase().trim()+f.substring(1);
+                                fields.add(f);
+                            }
+                        }
+                        do{
+                            Object obj=null;
+                            try{
+                                obj=cla.getConstructor().newInstance();
+                            }catch(Exception ex){
+                                ex.printStackTrace();
+                            }
+                            for(int i=0;i<num;i++){
+                                Object objval=rs.getObject(i+1);
+                                for(int n=0;n<fields.size();n++){
+                                    String fieldName=fields.get(n).trim();
+                                    if(columnNames.get(i).equals(fieldName.toLowerCase().trim())){
+                                        BeanUtils.copyProperties(obj, objval,fieldName);
+                                        break;
+                                    }
+                                }
+                            }
+                            list.add(obj);
+                        }while(rs.next());
+                    }catch(Exception ex){
+                        LOGGER.error(ex.toString());
+                    }
+                }
+            });
+        }catch(Exception ex){ex.printStackTrace();}
+        if(list.size()<=0){
+            return null;
+        }
+        return list;
+    }
+
+    /**
+     * 查询返回List<Map<String,Object>>格式数据,每一个Map代表一行数据，列名为key
+     * @param sql  sql语句
+     * @param params 填充问号占位符数
+     * @return
+     */
+    public List<Map<String,Object>> queryForMaps(String sql, Object[] params){
+        try{
+            if(params!=null&&params.length>0){
+                return jdbcTemplate.queryForList(sql, params);
+            }
+            return jdbcTemplate.queryForList(sql);
+        }catch(Exception ex){
+            LOGGER.error(ex.toString());
+            return null;
+        }
+    }
+
+    /**
+     * 获取数据总条数
+     * @param sql
+     * @param params
+     * @return
+     */
+    public Integer countColumn(String sql,Object[] params,boolean flag){
+        if(flag){
+            if(params!=null&&params.length!=0){
+                return jdbcTemplate.queryForObject(sql, params, Integer.class);
+            }
+                return jdbcTemplate.queryForObject(sql,Integer.class);
+        }
+        String rowsql="select count(*) from ("+sql+") gmtxtabs_";
+
+        return (Integer)queryOneColumnForSigetonRow(rowsql, params, Integer.class);
+    }
+
+    /**
+     * 查询分页（ORACLE数据库）
+     * @param sql     终执行查询的语句
+     * @param params  填充sql语句中的问号占位符数
+     * @param page    想要第几页的数据
+     * @param cla     要封装成的实体元类型
+     * @return        pageList对象
+     */
+    public PageList queryByPageForOracle(String sql, Object[] params, int page, int pageSize,Class cla) {
+        //查询总行数sql
+        int pages;
+        //总页数
+        int rows=countColumn(sql,params,false);
+        LOGGER.info("查询总行数为："+rows);
+        //查询总行数
+        //判断页数,如果是页大小的整数倍就为rows/pageRow如果不是整数倍就为rows/pageRow+1
+        if (rows % pageSize == 0) {
+            pages = rows / pageSize;
+        } else {
+            pages = rows / pageSize + 1;
+        }
+        StringBuffer paginationSQL = new StringBuffer(" SELECT * FROM ( ");
+        paginationSQL.append(" SELECT temp.* ,ROWNUM num FROM ( ");
+        paginationSQL.append(sql);
+        paginationSQL.append("　) temp where ROWNUM <= " + (page+1)*pageSize);
+        paginationSQL.append(" ) WHERE　num > " + page*pageSize);
+        //查询第page页的数据sql语句
+
+        LOGGER.debug(">>>>>>sql:"+paginationSQL.toString());
+        //查询第page页数据
+        List list=null;
+        if(cla!=null){
+            list=queryForObjectList(sql, params, cla);
+            LOGGER.debug("queryForObjectList:"+list.size());
+        }else{
+            list=queryForMaps(paginationSQL.toString(), params);
+            LOGGER.debug("queryForMaps:"+list.size());
+        }
+
+        //返回分页格式数据
+        PageList pl =new PageList();
+        pl.setPage(page);
+        //设置显示的当前页数
+        pl.setPageSize(pageSize);
+        //设置每页个数
+        pl.setPages(pages);
+        //设置总页数
+        pl.setList(list);
+        //设置当前页数据
+        pl.setTotalRows(rows);
+        //设置总记录数
+        return pl;
+    }
+
+    /**
+     * 获取所有数据不分页
+     * @param sql
+     * @param params
+     * @param cla
+     * @return
+     */
+    public PageList queryForOracle(String sql, Object[] params,Class cla) {
+        //查询总行数sql
+       // int pages;
+        //总页数
+        int rows=countColumn(sql,params,false);
+        LOGGER.info("查询总行数为："+rows);
+        List list;
+        if(cla!=null){
+            list=queryForObjectList(sql, params, cla);
+            LOGGER.debug("queryForObjectList:"+list.size());
+        }else{
+            list=queryForMaps(sql, params);
+            LOGGER.debug("queryForMaps:"+list.size());
+        }
+
+        //返回分页格式数据
+        PageList pl =new PageList();
+        pl.setPage(0);
+        //设置显示的当前页数
+        pl.setPageSize(0);
+        //设置每页个数
+        pl.setPages(0);
+        //设置总页数
+        pl.setList(list);
+        //设置当前页数据
+        pl.setTotalRows(rows);
+        //设置总记录数
+        return pl;
+    }
+
+    public List execSqlQuery(String sql) {
+        List<Map<String,Object>> list = null;
+        List<Object[]> objList = new ArrayList<>();
+        try {
+            list = jdbcTemplate.queryForList(sql);
+            for(Map<String,Object> map:list){
+                List<Object> collist = new ArrayList<>();
+                for(Map.Entry entry:map.entrySet()){
+                    collist.add(entry.getValue());
+                }
+                objList.add(collist.toArray());
+            }
+        }catch(Exception e){
+            throw e;
+        }
+        return objList;
+    }
+
+    public List execSqlCount(String sql) {
+        List<Map<String,Object>> list = null;
+        List objList = new ArrayList<>();
+        try {
+            list = jdbcTemplate.queryForList(sql);
+            for(Map<String,Object> map:list){
+                for(Map.Entry entry:map.entrySet()){
+                    objList.add(entry.getValue());
+                }
+            }
+        }catch(Exception e){
+            throw e;
+        }
+        return objList;
+    }
+
+    public int execSqlUpdate(String sql, Object... args) {
+        int ret = 0;
+        try {
+            ret = jdbcTemplate.update(sql, args);
+        }catch (Exception e){
+            throw e;
+        }
+        return ret;
+    }
+
+    private void printMap(Map<String,Object> line) {
+        for (Map.Entry<String, Object> entry : line.entrySet()) {
+            String key = entry.getKey();
+            LOGGER.debug(key + "=========" + entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/com/insigma/afc/msutil/dao/util/NodeUtil.java
@@ -0,0 +1,119 @@
+package com.insigma.afc.msutil.dao.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author by Xinshao,
+ * @Email xingshaoya@unittec.com,
+ * @Time on 2019/9/11 18:01.
+ * @Ticket :
+ */
+@Service
+public class NodeUtil {
+    private JdbcTemplateDao templateDao;
+
+
+    public NodeUtil(JdbcTemplate jdbcTemplate) {
+
+        templateDao = new JdbcTemplateDao(jdbcTemplate);
+
+    }
+
+    private static Map<Short,String> lines = new HashMap<>();
+
+    private static Map<Integer,String> stations = new HashMap<>();
+
+    private static Map<Long,String> devices = new HashMap<>();
+
+    private final String findLine = " select LINE_NAME from TMETRO_LINE where LINE_ID=";
+    private final String findStation = "select STATION_NAME from TMETRO_STATION where STATION_ID=";
+    private final String findDevice = "select DEVICE_NAME from TMETRO_DEVICE where DEVICE_ID=";
+
+    public  String getLineNameById(Short id){
+        String name = lines.get(id);
+        if(name == null){
+            Object list = templateDao.queryForString(
+                    findLine + id, null,"LINE_NAME");
+            String lineName = (String) list;
+            lines.put(id,lineName);
+            return lineName;
+        }
+        return name;
+    }
+    public  String getStationNameById(Integer id){
+        String name = stations.get(id);
+        if(name == null){
+            Object list = templateDao.queryForString(
+                    findStation + id, null,"STATION_NAME");
+            String lineName = (String) list;
+            stations.put(id,lineName);
+            return lineName;
+        }
+        return name;
+    }
+    public  String getDeviceNameById(Long id){
+        String name = devices.get(id);
+        if(name == null){
+            Object list = templateDao.queryForString(
+                    findDevice + id, null,"DEVICE_NAME");
+            String lineName = (String) list;
+            devices.put(id,lineName);
+            return lineName;
+        }
+        return name;
+    }
+    public String getTicktTypeName(Integer cardType){
+        if(cardType==null){
+
+            return null;
+        }
+        Object list = templateDao.queryForString("select t.CARDTYPENAME from V_TICKET_TYPE t where 1=1 and t.CARDTYPE=" + cardType + " order by CARDTYPE",
+                null,"CARDTYPENAME");
+
+        return list+"";
+
+    }
+    public Map<String,Object> getTicktTypeName(){
+            List list = templateDao.queryForMaps("select t.CARDTYPE, t.CARDTYPENAME from V_TICKET_TYPE t where 1=1  order by CARDTYPE",
+                    null);
+            return (Map) list;
+        }
+     public String getUserNameById(String useId,boolean flag){
+        String sql;
+        if (flag){
+            sql = "select USER_NAME from TAP_USER where USER_ID="+useId+" and IF_VALID=0";
+        }else {
+            sql = "select USER_NAME from TAP_USER where USER_ID="+useId;
+        }
+         Object list = templateDao.queryForString(sql, null,"USER_NAME");
+        if(list!=null){
+            return list+"";
+        }
+         return "未知";
+     }
+
+     public String getTimeText(String time){
+        if(time.length()!=6){
+            return null;
+        }
+        return time.substring(0,2)+":"+time.substring(2,4)+":"+time.substring(4);
+
+     }
+
+    public String getLineTextById(Short id){
+        return getLineNameById(id)+"/"+id;
+    }
+    public String getStationTextById(Integer id){
+        return getStationNameById(id)+"/"+id;
+    }
+    public String getDeviceTextById(Long id){
+        return getDeviceNameById(id)+"/"+id;
+    }
+
+}
